4a5384b64275c016f49027cc4f5dfe19030e0949
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2024-08-18 23:08:56 -0600

    Estilos agregados a la barra de navegación

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edd n' dev</title>
+    <title>Compra seguro</title>
     <link rel="stylesheet" href="build/css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
