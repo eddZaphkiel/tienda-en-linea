65937b530b23e75c749309202afe50d21e3e78d6
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2024-08-18 20:32:08 -0600

    Configurando el ambiente de desarrollo para empezar a trabajar en el proyecto

--- controllers/siteController.php
+++ controllers/siteController.php
@@ -5,28 +5,5 @@ namespace Controllers;
 use MVC\Router;
 
 class siteController {
-
-    public static function index(Router $router) {
-        $router->render('site/index');
-    }
-
-    public static function aboutMe(Router $router) {
-        $router->render('site/about');
-    }
-
-    public static function mySkills(Router $router) {
-        echo 'My skills';
-    }
-
-    public static function myProjects(Router $router) {
-        echo 'My projects';
-    }
-
-    public static function contactMe(Router $router) {
-        echo 'Contact me';
-    }
-
-    public static function blog(Router $router) {
-        echo 'Blog';
-    }
+    
 }
