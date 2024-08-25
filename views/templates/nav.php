fe10cd4c8c955e7864f1b6933a821186ebcf4146
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2024-08-24 20:17:32 -0600

    Algunos cambios al layout

--- views/templates/nav.php
+++ views/templates/nav.php
@@ -1,11 +1,12 @@
 <div class="nav">
+    <div class="nav_brand">
+        <a href="/" class="nav_brand_link"></a>
+    </div>
     <div class="nav_menu">
         <a href="/" class="nav_menu_link">Inicio</a>
         <a href="/freq" class="nav_menu_link">Preguntas frecuentes</a>
         <a href="/#contacto" class="nav_menu_link">Contacto</a>
-        <button class="nav_menu_respond">
-            svg
-        </button>
     </div>
-
+    <button class="nav_menu_respond">
+    </button>
 </div>
